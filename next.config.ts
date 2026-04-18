fae29235d34381b28a1810472422165d1aea2a33
Mark Boyle <dev@grimlamb.com>
2026-04-18 15:59:12 +0100

    Initial Next.js setup for GitHub Pages

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+   output: 'export',
+  basePath: '/bf-hol-26',
+  assetPrefix: '/bf-hol-26/',
+};
+
+export default nextConfig;
